0a53e30fc8893d25575f5148f9f4691936639253
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-06-26 01:06:55 +0530

    add script to run tests

---
+++ run-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_06.jdk/Contents/Home/
+
+function each_test {
+  ./clean.sh && lein2 clean
+  lein2 with-profile dev,$1 test
+  lein2 clean && ./clean.sh
+}
+
+if [ -z "$1" ]; then
+  each_test 1.4
+elif [ "$1" == "all" ]; then
+  for a in 1.2 1.3 1.4 1.5; do
+    each_test $a
+  done
+else
+  for a in $1 $2 $3 $4 $5 $6; do
+    each_test $a
+  done
+fi
+
