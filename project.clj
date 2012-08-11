5079e8fe46fd235ce18ccdb61a2fb344194183ee
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-08-11 13:37:40 +0530

    update dependency for Clojure 1.5.0-alpha3

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-alpha1"]]}}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}}
   :aliases {"dev"     ["with-profile" "dev,1.4"]
             "all"     ["with-profile" "dev,1.2:dev,1.3:dev,1.4:dev,1.5"]
             "reflect" ["assoc" ":warn-on-reflection" "true" "compile"]}
